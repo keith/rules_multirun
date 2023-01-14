82066a5dc7e88b1ef7a9781c56bf104b9ddd6f77
Keith Smiley <keithbsmiley@gmail.com>
2023-01-14 14:58:41 -0800

    Add runfiles in tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,29 +2,47 @@
 
 set -euo pipefail
 
-output=$(./tests/hello.bash)
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+script=$(rlocation rules_multirun/tests/hello.bash)
+output=$($script)
 if [[ "$output" != "hello" ]]; then
   echo "Expected 'hello', got '$output'"
   exit 1
 fi
 
-./tests/validate_args_cmd.bash
-./tests/validate_env_cmd.bash
+script=$(rlocation rules_multirun/tests/validate_args_cmd.bash)
+$script
+script=$(rlocation rules_multirun/tests/validate_env_cmd.bash)
+$script
 
-parallel_output="$(./tests/multirun_parallel.bash)"
+script="$(rlocation rules_multirun/tests/multirun_parallel.bash)"
+parallel_output="$($script)"
 if [[ -n "$parallel_output" ]]; then
   echo "Expected no output, got '$parallel_output'"
   exit 1
 fi
 
-serial_output=$(./tests/multirun_serial.bash | sed 's/@@/@/g')
+script=$(rlocation rules_multirun/tests/multirun_serial.bash)
+serial_output=$($script | sed 's/@@/@/g')
 if [[ "$serial_output" != "Running @//tests:validate_args_cmd
 Running @//tests:validate_env_cmd" ]]; then
   echo "Expected labeled output, got '$serial_output'"
   exit 1
 fi
 
-serial_no_output=$(./tests/multirun_serial_no_print.bash)
+script=$(rlocation rules_multirun/tests/multirun_serial_no_print.bash)
+serial_no_output=$($script)
 if [[ -n "$serial_no_output" ]]; then
   echo "Expected no output, got '$serial_no_output'"
   exit 1
