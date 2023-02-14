0a215a4e5c7d2f7fc6bf63afd25360a6975ec6cc
Andreas Herrmann <andreash87@gmx.ch>
2023-02-14 20:26:53 +0100

    Support change-dir before calling a command (#14)
    Changes the bash script generated by `command` to look up the target
    executable in the runfiles.
    
    Closes #13

---
+++ tests/validate-chdir-location.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+set -euo pipefail
+
+WORKDIR="$(mktemp -d)"
+trap 'rm -rf -- "$WORKDIR"' EXIT
+cd "$WORKDIR"
+
+if [[ $# != 1 ]]; then
+  echo "error: expected one argument, got $#"
+  exit 1
+fi
+
+hello="$(rlocation "$1")"
+output="$($hello)"
+
+if [[ "$output" != hello ]]; then
+  echo "error: expected '$hello' to print 'hello', got '$output'"
+  exit 1
+fi
